51a9f2b62a4877b3eb417d69b6274f84c392a787
Wijnand Modderman-Lenstra <maze@pyth0n.org>
2013-02-13 10:49:11 +0100

    Different post script handling, supply the list of updates as a file argument

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup, Extension
+
+setup(
+    name         = 'yummie',
+    version      = '0.1.0',
+    author       = 'Wijnand Modderman-Lenstra',
+    author_email = 'maze@pyth0n.org',
+    description  = 'Automated yum upgrades',
+    license      = 'MIT',
+    keywords     = 'yum rpm update upgrade',
+    scripts      = ['yummie'],
+    data_files   = (
+        ('/etc/yum/yummie.conf', 'yummie.conf.example'),
+    ),
+)
